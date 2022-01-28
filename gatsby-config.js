2c15c08aed6a4306ac5eda97b19d500350531cfd
therealemjy <maxime.julian.creations@gmail.com>
2022-01-28 10:33:41 -0500

    initialize Gatsby project

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+      title: `Make it rain`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-react-helmet"]
+};
