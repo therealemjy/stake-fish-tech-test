a9bf0a981620e3d287204bcee6214ccd930a18ff
therealemjy <maxime.julian.creations@gmail.com>
2022-01-29 10:02:41 -0500

    add promise and fetch polyfills

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,9 @@
+// Import promise polyfill
+import 'promise-polyfill/src/polyfill';
+
+// Import fetch polyfill
+import 'whatwg-fetch';
+
 // Import fonts
 import '@fontsource/inter/400.css';
 import '@fontsource/inter/700.css';
